b47d4dc88015f7b3c7dff50a9612c0f46ce76668
Omkar <omkarmhatre4@gmail.com>
2024-02-17 22:25:43 +0530

    Retrieving of all packages and particular Package Details

--- src/main/java/com/cms/controller/PackageController.java
+++ src/main/java/com/cms/controller/PackageController.java
@@ -9,6 +9,7 @@ import com.cms.entity.Account;
 import com.cms.entity.PackageDetails;
 import com.cms.entity.PackageEntity;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -149,6 +150,20 @@ public class PackageController {
             }
             return ResponseEntity.ok(packages);
         } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+    }
+
+    @GetMapping("/details/{id}")
+    public ResponseEntity<PackageDetails> getPackageDetails(@PathVariable Long id){
+        try{
+            Optional<PackageDetails> packageDetails = packageDetailsRepository.findById(id);
+            if(packageDetails.isEmpty()){
+                return ResponseEntity.notFound().build();
+            }
+            return ResponseEntity.ok(packageDetails.get());
+
+        }catch(Exception e){
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         }
     }
